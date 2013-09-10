cc00c5e01b12e6f73232f84334902950a742daab
Eric <ehannum@gmail.com>
2013-09-10 16:39:45 -0700

    AboutObjects.js part 3
    This one is all out of order, I have no idea what part I'm really on.
    All I know is iI have done 3 so far in the order they appear in the .js
    file...

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -44,8 +44,8 @@ describe("About Objects", function () {
       }
     };
    
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(meglomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2013);
+    expect(meglomaniac.calculateAge()).toBe(43);
   });
 
   describe("'in' keyword", function () {
